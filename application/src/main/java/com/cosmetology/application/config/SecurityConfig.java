890d380b0a4c8cd994df62c0c06a51b1eb766add
Zakhar <zverem@softserveinc.com>
2021-09-27 21:09:11 +0300

    Security change

--- application/src/main/java/com/cosmetology/application/config/SecurityConfig.java
+++ application/src/main/java/com/cosmetology/application/config/SecurityConfig.java
@@ -50,7 +50,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity http)throws Exception {
-        http.cors().and().csrf().disable()
+        http.csrf().disable()
                 .authorizeRequests().antMatchers("/**").permitAll()
                 .antMatchers("/v2/api-docs","/configuration/**","/swagger*/**","/webjars/**").permitAll()
                 .anyRequest()
